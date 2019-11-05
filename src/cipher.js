c93f413905bc336bfe10b371b481a2d338308c85
MonicaMartz <monik92_ms@hotmail.com>
2019-11-05 09:23:29 -0600

    html y css completos

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,29 @@
 window.cipher = {
   // Acá escribe tu código//
-  };
 
-function obtenerNipAScii() {
-  let nip = document.getElementById("keyone").value;
-  nip.charCodeAt
+
+function (string,offset);{
+
+
+  let NuevaContraseña = ""  //variable de la nueva contraseña
+
+  //for (let i=0; i<text.length; i++){   //ciclo para evaluar cada letra
+
+  let evalua = text [i];//
+
+
+
+
+
+  }
+
+
+
+
+text.chardCodeAt(i) =
+for (i=0,i<text.lenght,i++)
+
+
+
+
+   };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 
+//let offset = document.getElementById("displace").value;//
+
+//let newKey = document.getElementById("keyone").value;
+
+//document.getElementById("keytwo").innerHTML //Regresar el valor nueva contraseña al cuadro de texto
+
+let letra = "A";
+let num= 2;
+let desplaza;
+
+function convertir (){
+  desplaza= (letra.charCodeAt(0)- 65 + num)%26 + 65;
+
+console.log(String.fromCharCode(desplaza));
+}
+
+document.getElementById("desplaza").innerHTML
