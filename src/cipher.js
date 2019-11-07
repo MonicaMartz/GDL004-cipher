4e09f999ea08545b11b162090a091f89b3a4ce3b
MonicaMartz <monik92_ms@hotmail.com>
2019-11-07 12:44:43 -0600

    ejecuta cifrado y decifrado. modificacion en formula decifrado

--- src/cipher.js
+++ src/cipher.js
@@ -2,26 +2,32 @@ window.cipher = {
   // Acá escribe tu código//
  encode: (offset,string) => {
 
- let resultado="";
+ let resultado=[];
 
     for(i = 0; i < string.length; i++) {
       //resultado= resultado + (String.fromCharCode((string.charCodeAt(i)-65 + offset)%26 + 65));
-      let enAscii=string.charCodeAt(i)
+      let enAscii=string.charCodeAt(i);
       if(enAscii>= 65 && enAscii <= 90){
-        resultado= resultado + (String.fromCharCode((enAscii)-65 + offset)%26 + 65));
-      } else if(>65){
-        alert("Recuerda, sólo Mayusculas")
-
-      } else {
-          alert("Recuerda, sólo Mayusculas")
+        resultado.push(String.fromCharCode((enAscii -65 + offset)%26 + 65));
         }
           }
-    console.log(resultado);
-  }
+    return resultado.join("");
+  },
 
 
+  decode: (offset,string) => {
+  console.log(offset,string);
+  let resultado2="";
 
+    for(i =0; i < string.length; i++) {
+      resultado2= resultado2 + (String.fromCharCode((string.charCodeAt(i)-65 - offset)%26 + 65));
+    //  resultado2= resultado2 + (String.fromCharCode((string.charCodeAt(i)+90 - offset)%26 + 0));
 
-
-
+    }
+console.log(resultado2);
+ return resultado2;
+}
    };
+
+
+//}
--- src/index.js
+++ src/index.js
@@ -2,58 +2,39 @@
 
 
 
-window.cipher.encode(offset,newKey);
+//función para encriptar
 
 function encripta(){
+
   let offset= parseInt(document.getElementById("displace").value);
   let string = document.getElementById("keyone").value;
-  let nuevaContrasena = cipher.encode(offset,string);
+  let nuevaContrasena = window.cipher.encode(offset,string);
 
-  document.getElementById("keytwo").innerHTML=nuevaContrasena;
+  //el window.cipher.encode(offset,string) me envia las variables offset y string al cipher.js
 
+  document.getElementById("keytwo").value=nuevaContrasena;
+//console.log(nuevaContrasena);
 }
-document.getElementById("enviar").addEventListener("click",nuevaContrasena);
+document.getElementById("enviar").addEventListener("click",encripta);
 //posibilidad de ir dentro del corchete
 
 
-//
-//let newKey;
-//let offset;
-//let resultado="";
-//declaré las variables arriba y las mandé a llamar con un
-//valor dentro de la funcion pues de lo contrario no reconocia a las variables
-//ahora haré la prueba desde el doc cipher.js
-//function convert (){
-//newKey = document.getElementById("keyone").value;
-//offset = parseInt(document.getElementById("displace").value);
-
-window.cipher.encode(offset,newKey);
 
 
-  for(i = 0; i < newKey.length; i++) {
+function desencripta(){
 
-    resultado= resultado + (String.fromCharCode((newKey.charCodeAt(i)-65 + offset)%26 + 65));
-  }
-  console.log(resultado);
-  }
+  let offset= parseInt(document.getElementById("displace").value);
+  let string= document.getElementById("keythree").value;
+  let nuevaContrasena2 = window.cipher.decode(offset,string);
 
+  document.getElementById("keyfour").value=nuevaContrasena2;
 
-
-  //document.getElementById("keytwo").innerHTML //Regresar el valor nueva contraseña al cuadro de texto
-//prueba 1 basica con letras
-let letra = "ABC";
-let num= 2;
-let desplaza="";
-
-function convertir (){
-
-for( i = 0; i < letra.length; i++) {
-desplaza= desplaza + (String.fromCharCode((letra.charCodeAt(i)- 65 + num)%26 + 65));
 }
-console.log(desplaza);
-}
+document.getElementById("enviar2").addEventListener("click",desencripta);
 
-prueba en letras
+
+
+
 let letra2 = "C";
 let num2 = 2;
 let reemplaza="";
@@ -67,3 +48,35 @@ function revertir (){
 }
 console.log(reemplaza);
 }
+
+
+
+
+
+//prueba 1 basica encriptado con letras
+//let letra = "ABC";
+//let num= 2;
+//let desplaza="";
+
+//function convertir(){
+
+//for( i = 0; i < letra.length; i++) {
+//desplaza= desplaza + (String.fromCharCode((letra.charCodeAt(i)- 65 + num)%26 + 65));
+//}
+//console.log(desplaza);
+//}
+
+//prueba desencriptar en letras
+//let letra2 = "C";
+//let num2 = 2;
+//let reemplaza="";
+
+//function revertir (){
+
+  //for( i =0; i<letra2.length; i++) {
+
+   //reemplaza= reemplaza + (String.fromCharCode((letra2.charCodeAt(i)- 65 - num2)%26 + 65));
+
+//}
+//console.log(reemplaza);
+//}
